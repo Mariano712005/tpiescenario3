de0497248c7d7fbd6a77b9620bfc93041baa687d
Mariano712005 <retamosomariano7@gmail.com>
2025-06-22 12:46:42 -0300

    producto.h

--- producto.h
+++ producto.h
@@ -13,18 +13,18 @@ typedef struct {
 
 typedef struct Nodo {
     Producto producto;
-    struct Nodo* siguiente;
+    struct Nodo * siguiente;
 } Nodo;
 
-Nodo* agregarProducto(Nodo* cabeza, Producto prod);
-Nodo* eliminarProducto(Nodo* cabeza, int codigo);
-void modificarProducto(Nodo* cabeza, int codigo);
-Producto* buscarPorCodigo(Nodo* cabeza, int codigo);
-Nodo* buscarPorNombre(Nodo* cabeza, const char* nombre);
-void mostrarProductos(Nodo* cabeza);
+Nodo * agregarProducto(Nodo * cabeza, Producto prod);
+Nodo * eliminarProducto(Nodo * cabeza, int codigo);
+void modificarProducto(Nodo * cabeza, int codigo);
+Producto * buscarPorCodigo(Nodo * cabeza, int codigo);
+Nodo * buscarPorNombre(Nodo * cabeza, const char * nombre);
+void mostrarProductos(Nodo * cabeza);
 void mostrarProducto(Producto prod);
-void ordenarPorPrecio(Nodo* cabeza);
-void guardarInventario(Nodo* cabeza);
-Nodo* cargarInventario();
+void ordenarPorPrecio(Nodo * cabeza);
+void guardarInventario(Nodo * cabeza);
+Nodo * cargarInventario();
 
 #endif
